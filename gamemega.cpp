438a386b1121796acf75d186bb78395735373d16
YOUSSEF <youssef.ebrahim.mohsen@gmail.com>
2025-03-24 20:09:41 +0200

    love you so

--- gamemega.cpp
+++ gamemega.cpp
@@ -3,9 +3,8 @@ using namespace std;
 int main()
 {
 	cout << "Hello World"<<endl;
-	for (int i = 0; i < 10; i++)
-	{
-		cout << "ki" << endl;
-	}
+	for (int i = 0; i < 10; i++){ cout << "ki" << endl; }
+	for (int i = 0; i < 10; i++) { cout << "fjhkdjfh" << endl; }
+
 	return 0;
 }
